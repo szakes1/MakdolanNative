12b97ee92f099bccd8dba43344279325f8fe3924
Kamil Kras <kamilox26@gmail.com>
2019-08-12 10:21:03 +0200

    Add immersive mode to generated coupon activity

--- app/src/main/java/com/szakes1/makdolannative/activities/GeneratedCouponActivity.kt
+++ app/src/main/java/com/szakes1/makdolannative/activities/GeneratedCouponActivity.kt
@@ -7,6 +7,7 @@ import com.szakes1.makdolannative.R
 import com.szakes1.makdolannative.helpers.Makdolan
 import kotlinx.android.synthetic.main.activity_generated_coupon.*
 import net.danlew.android.joda.JodaTimeAndroid
+import android.view.View
 
 class GeneratedCouponActivity : AppCompatActivity() {
 
@@ -23,5 +24,20 @@ class GeneratedCouponActivity : AppCompatActivity() {
         val couponImage = intent.getIntExtra("coupon_image", 0)
 
         generated_coupon_IMGV.setImageDrawable(ContextCompat.getDrawable(applicationContext, couponImage))
+
+        //Enables immersive mode
+        layout.systemUiVisibility = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN
+                )
+    }
+
+    //Enables immersive mode if activity has focus
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) layout.systemUiVisibility = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN
+                )
     }
 }
