13c622d92fb16bfadb9497ac55e1dc8fb2a218a4
Karol Z <karol.zientek001@protonmail.com>
2019-08-13 17:13:16 -0500

    Added icons to shortcuts and Pinch To Zoom gesture
    Added icon for each shortcut and Pinch To Zoom gesture for GeneratedCouponActivity

--- app/src/main/java/com/szakes1/makdolannative/activities/GeneratedCouponActivity.kt
+++ app/src/main/java/com/szakes1/makdolannative/activities/GeneratedCouponActivity.kt
@@ -2,14 +2,21 @@ package com.szakes1.makdolannative.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.ScaleGestureDetector
 import android.view.View
 import androidx.core.content.ContextCompat
 import com.szakes1.makdolannative.R
 import com.szakes1.makdolannative.helpers.Makdolan
+import com.szakes1.makdolannative.helpers.OnPinchZoomListener
 import kotlinx.android.synthetic.main.activity_generated_coupon.*
 import net.danlew.android.joda.JodaTimeAndroid
 
 class GeneratedCouponActivity : AppCompatActivity() {
+    private lateinit var mScaleGestureDetector: ScaleGestureDetector
+    private lateinit var zoomableView: View
+    private var mScaleFactor: Float = 1.0f
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_generated_coupon)
@@ -22,17 +29,29 @@ class GeneratedCouponActivity : AppCompatActivity() {
 
         val couponImage = intent.getIntExtra("coupon_image", 0)
         generated_coupon_IMGV.setImageDrawable(ContextCompat.getDrawable(applicationContext, couponImage))
+
         // Enables immersive mode
         generatedCouponWrapperLayout.systemUiVisibility = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                                          or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                                          or View.SYSTEM_UI_FLAG_FULLSCREEN)
+                                                            or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                                                            or View.SYSTEM_UI_FLAG_FULLSCREEN)
+
+        // Add Pinch to Zoom gestures
+        zoomableView = findViewById(R.id.generatedCouponWrapperLayout)
+        mScaleGestureDetector = ScaleGestureDetector(this, OnPinchZoomListener(mScaleFactor, zoomableView))
     }
 
     // Enables immersive mode if activity is focused
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
         if (hasFocus) generatedCouponWrapperLayout.systemUiVisibility = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                                                        or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                                                        or View.SYSTEM_UI_FLAG_FULLSCREEN)
+                                                                          or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                                                                          or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    }
+
+    // Listen for touch events
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        mScaleGestureDetector.onTouchEvent(event)
+
+        return true
     }
 }
